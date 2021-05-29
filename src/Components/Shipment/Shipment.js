4dd718516292884e1d0efbfa9248fb25b501b9f5
smriad <skmdriad@gmail.com>
2021-05-29 23:56:11 +0800

    sign out added

--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
-    return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
-    );
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); 
+
+  return (
+    
+    <form onSubmit={handleSubmit(onSubmit)}>
+     
+      <input defaultValue="test" {...register("example")} />    
+      
+      <input {...register("exampleRequired", { required: true })} />      
+      {errors.exampleRequired && <span>This field is required</span>}
+      
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
